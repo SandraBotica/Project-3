6ea8c99975cf8a69191be1f0c0b44c6afed3b85b
Cindy Wong <cwsynyi88@gmail.com>
2023-04-09 18:14:38 +0800

    consolidate top 5 charts

--- coffee/static/top_5_import_export.js
+++ coffee/static/top_5_import_export.js
@@ -1,10 +1,14 @@
 async function getPlotData() 
 {
     let chartdata;
+    let chartdata2;
 
     // make API call to get the top 5 import
     const response = await fetch('/api/v1.0/data/top_5_import');
     chartdata = await response.json();
+
+    const response2 = await fetch('/api/v1.0/data/top_5_export');
+    chartdata2 = await response2.json();
     
     // set trace1
     var trace1 = 
@@ -15,14 +19,22 @@ async function getPlotData()
         name: 'Top 5 Import by Country'
     }
 
-    var data = [trace1];
+    var trace2 = 
+    {
+        x: chartdata2.map(d => d.Country),
+        y: chartdata2.map(d => d.total_exported),
+        type: 'bar',
+        name: 'Top 5 Export by Country'
+    }
+
+    var data = [trace1, trace2];
 
     const layout = {
-        title: 'Top 5 Import by Country (1990 - 2019)',
+        title: 'Top 5 Import & Export by Country (1990 - 2019)',
         xaxis: { title: 'Year' },
         yaxis: { title: 'Thousands 60kg bags' }
     };
 
-    Plotly.newPlot('top_5_import', data, layout);
+    Plotly.newPlot('top_5_import_export', data, layout);
 }
 getPlotData();
